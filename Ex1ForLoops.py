9ffa69dd46e31f7e7d103e8294a1b7df68b15248
Leah Moore <LMoore@spartaglobal.com>
2019-10-30 10:17:36 +0000

    Commit 1

---
+++ Ex1ForLoops.py
@@ -0,0 +1,41 @@
+# FOR LOOP STRUCTURE
+
+#  A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string)
+#
+# list_data = [1, 2, 3, 4, 5]
+# for num in list_data:
+#     print(num * 2)
+#
+# embedded_lists = [[1, 2, 3],[4, 5, 6]]
+# for data in embedded_lists:
+#     print(data)
+
+# LOOPS FOR DICTIONARIES
+
+# dict_data = {1: {"name": "Bronson", "money": "$0.05"},
+#              2: {"name": "Masha", "money": "$3.66"},
+#              3: {"name": "Roscoe", "money": "$1.14"}}
+# Fetching key values
+
+# for k in dict_data:
+#     print(k)
+#
+# #Fetching values from the dictionary
+#
+# for item in dict_data.values():
+#     print(item)
+#
+# for items in dict_data.values():
+#     print(items["money"])
+
+# List for and if
+
+# list_data = [1, 2, 3, 4, 5]
+
+# for num in list_data:
+#     if num == 3:
+#         print("I found three")
+#     elif num > 3:
+#         print("gone too far")
+#     else:
+#         print("too soon")
