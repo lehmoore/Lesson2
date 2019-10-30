4571c0414457a08233ae54005b11da88ae64bc56
Leah Moore <LMoore@spartaglobal.com>
2019-10-30 13:27:22 +0000

    Commit 5

--- fun1.py
+++ fun1.py
@@ -1,3 +1,29 @@
 # FUNCTIONS
 
-# 
+# A special relationship where each input has a single output
+
+# input ---> BLACKBOX ---> Output
+
+# Function without arguments
+# def func():
+#     print("Hello")
+# func()
+
+ # Function with arguments
+
+# def func(a, b):
+#      return a + b
+# print("The sum is", func(10,20), ".")
+
+# Function with default arguments
+
+# def func(a = 10, b = 30):
+#     return a + b
+# print("The sum is", func(), ".")
+
+# Function with default and again passing the values
+
+# def func(a = 10, b = 30):
+#     return a + b
+# print("The sum is", func(10, 40), ".")
+
